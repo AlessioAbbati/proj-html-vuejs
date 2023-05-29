74e7c25a30cf7e09af24cb0abcc193c95602c7cf
AlessioAbbati <alex_117@live.it>
2023-05-29 12:48:15 +0200

    add project, structure and mod favicon

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+import AppFooter from './components/AppFooter.vue'
+import AppHeader from "./components/AppHeader.vue"
+import AppMain from './components/AppMain.vue'
+
+export default {
+  components: { 
+    AppHeader,
+    AppMain,
+    AppFooter,
+  },
+}
+
+
+</script>
+
+<template>
+  <div class="container">
+    <app-header />
+    <app-main />
+    <app-footer />
+  </div>
+</template>
+
+<style lang="scss">
+
+</style>
