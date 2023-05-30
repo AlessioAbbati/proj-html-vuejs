31b9364d816fa8d159f126c8cd14bf274cd507d7
AlessioAbbati <alex_117@live.it>
2023-05-30 13:39:31 +0200

    add footer structure

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,12 +1,44 @@
-<script>
-
-</script>
+<script></script>
 
 <template>
-  
+  <footer>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-3">
+                <div class="content d-flex flex-column">
+                    
+                </div>              
+            </div>
+            <div class="col-md-5">
+                <div class="content">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque voluptatibus eius libero? Ab, tenetur laborum quis ipsa rem sunt, amet ut, quisquam eos accusantium dolore aperiam vel obcaecati officiis commodi.
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="content">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque voluptatibus eius libero? Ab, tenetur laborum quis ipsa rem sunt, amet ut, quisquam eos accusantium dolore aperiam vel obcaecati officiis commodi.
+                </div>
+            </div>
+            <div class="col-md-2">
+                <div class="content">
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque voluptatibus eius libero? Ab, tenetur laborum quis ipsa rem sunt, amet ut, quisquam eos accusantium dolore aperiam vel obcaecati officiis commodi.
+                </div>
+            </div>
+        </div>
+    </div>
+  </footer>
 </template>
 
 <style lang="scss" scoped>
-
+footer {
+    height: 25rem;
+    width: 100%;
+    background-image: url('../assets/img/footer-bg-football.jpg');
+    background-size: cover;
+    .content {
+        color: white;
+        border: 1px solid white;
+    }
+}
 </style>
 
