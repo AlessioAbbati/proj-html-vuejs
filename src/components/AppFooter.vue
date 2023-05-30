81a2d4d282e9fbfbc7c12f1048f21789f27a3154
AlessioAbbati <alex_117@live.it>
2023-05-30 13:55:50 +0200

    add first part of footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,22 +6,44 @@
         <div class="row">
             <div class="col-md-3">
                 <div class="content d-flex flex-column">
-                    
+                    <div class="img">
+                        <img src="../assets/img/logo-footer-football.png" alt="">
+                    </div>
+                    <div class="info">
+                        <span class="d-flex align-items-center justify-content-start">
+                            <font-awesome-icon :icon="['fas', 'location-dot']" />
+                            <font-awesome-icon :icon="['fas', 'location-pin']" />
+                            <p>New Jersey, USA</p>
+                        </span>
+                        <span class="d-flex align-items-center justify-content-start">
+                            <font-awesome-icon :icon="['fas', 'phone-volume']" />
+                            <p>+1 (234) 567 89 10</p> 
+                        </span>
+                        <span class="d-flex align-items-center justify-content-start">
+                            <font-awesome-icon :icon="['far', 'envelope']" />
+                            <p>example@example.com</p>
+                        </span>
+                    </div>
+                    <div class="socials d-flex justify-content-around">
+                        <font-awesome-icon class="icon" :icon="['fab', 'facebook-f']" />
+                        <font-awesome-icon class="icon" :icon="['fab', 'instagram']" />
+                        <font-awesome-icon class="icon" :icon="['fab', 'twitter']" />
+                    </div>
                 </div>              
             </div>
             <div class="col-md-5">
                 <div class="content">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque voluptatibus eius libero? Ab, tenetur laborum quis ipsa rem sunt, amet ut, quisquam eos accusantium dolore aperiam vel obcaecati officiis commodi.
+                    
                 </div>
             </div>
             <div class="col-md-2">
                 <div class="content">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque voluptatibus eius libero? Ab, tenetur laborum quis ipsa rem sunt, amet ut, quisquam eos accusantium dolore aperiam vel obcaecati officiis commodi.
+                    
                 </div>
             </div>
             <div class="col-md-2">
                 <div class="content">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eaque voluptatibus eius libero? Ab, tenetur laborum quis ipsa rem sunt, amet ut, quisquam eos accusantium dolore aperiam vel obcaecati officiis commodi.
+                    
                 </div>
             </div>
         </div>
@@ -38,6 +60,18 @@ footer {
     .content {
         color: white;
         border: 1px solid white;
+        .info {
+            span {
+                vertical-align: middle;
+            }
+        }
+        .socials {
+            font-size: 1.5rem;
+            .icon {
+                border: 1px solid white;
+                
+            }
+        }
     }
 }
 </style>
