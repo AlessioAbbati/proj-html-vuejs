3d1875bdf9ad8d4aa0b28a6bc05ec326f5dc6469
AlessioAbbati <alex_117@live.it>
2023-05-31 11:49:24 +0200

    add header carousel

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -33,6 +33,28 @@ export default {
                     title: 'CONTACT',
                     link: '#', 
                 },
+            ],
+            arrSlider: [
+                {
+                    slide: 'src/assets/img/slider1-1.jpg',
+                    title: 'Best Game In The World',
+                    info: ''
+                },
+                {
+                    slide: 'src/assets/img/slider2-1.jpg',
+                    title: 'Sport Club',
+                    info: ''
+                },
+                {
+                    slide: 'src/assets/img/slider3.jpg',
+                    title: 'Football Club',
+                    info: 'Private football matches'
+                },
+                {
+                    slide: 'src/assets/img/slider4.jpg',
+                    title: 'I Just Can Get Enough',
+                    info: 'Join the club'
+                },
             ]
             
         }
@@ -45,13 +67,13 @@ export default {
         <div class="black">
             <div class="logos d-flex align-items-center justify-content-center">
                 <div class="img">
-                        <img src="../assets/img/sponsor1.png" alt="">
+                    <img src="../assets/img/sponsor1.png" alt="">
                 </div>
                 <div class="img">
-                        <img src="../assets/img/sponsor2.png" alt="">
+                    <img src="../assets/img/sponsor2.png" alt="">
                 </div>
                 <div class="img">
-                        <img src="../assets/img/logo-football.png" alt="">
+                    <img src="../assets/img/logo-football.png" alt="">
                 </div>
                 <font-awesome-icon class="icon" :icon="['fab', 'facebook-f']" />
                 <font-awesome-icon class="icon" :icon="['fab', 'instagram']" />
@@ -59,83 +81,68 @@ export default {
             </div>
             <nav>
                 <ul class="list-unstyled d-flex justify-content-center">
-                    <app-nav 
-                        v-for="title in arrList" 
-                        :key="title.title" 
-                        :title="title.title" 
-                        :link="title.link"  
-                    />
+                    <app-nav v-for="title in arrList" :key="title.title" :title="title.title" :link="title.link" />
                 </ul>
             </nav>
         </div>
-        <div class="carousel d-flex justify-content-between align-items-center">
-            <div class="chevron_circle">
-                <font-awesome-icon class="chevron" :icon="['fas', 'chevron-left']" />
+        <div id="carouselExample" class="carousel slide">
+            <div class="carousel-inner">
+                <div class="carousel-item active" v-for="(slide, index) in arrSlider" :key="index">
+                    <img :src="slide.slide" class="d-block w-100" alt="...">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h1>{{ slide.title }}</h1>
+                        <p>{{ slide.info }}</p>
+                        <button>Learn more &rightarrow;</button>
+                    </div>
+                </div>
             </div>
-            <div class="text d-flex flex-column align-items-center">
-                <h1>Football Club</h1>
-                <h1>Sport club</h1>
-                <small>Private football matches</small>
-                <button><small>Learn More</small></button>
-            </div>
-            <div class="chevron_circle">
-                <font-awesome-icon class="chevron" :icon="['fas', 'chevron-right']" />
-            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
         </div>
     </header>
 </template>
 
 <style lang="scss" scoped>
 header {
-    height: 40rem;
     width: 100%;
-    background-image: url('../assets/img/slider2-1.jpg');
-    background-size: cover;
+    position: relative;
 
     .black {
         background-color: rgba($color: #000000, $alpha: 0.5);
+        z-index: 9;
+        position: absolute;
+        top: 0;
+        right: 0;
+        left: 0;
         .icon {
             font-size: 2rem;
             margin-left: 10px;
             color: white;
         }
     }
-    .carousel {
-        margin-top: 3rem;
-        .text {
-        color: white;
-        h1 {
-            font-size: 4rem;
-            font-weight: 700;
-        }
-        button {
-            background-color: transparent;
-            border-radius: 1.3rem;
-            padding: .7rem;
-            color: white;
-            border: 2px solid white;
-            margin-top: 1rem;
-            font-weight: 600;
-        }
-    }
-        .chevron_circle {
-            position: relative;
-            width: 70px;
-            height: 70px;
-           .chevron {
+    .carousel-item {
+        position: relative;
+        .carousel-caption {
+            position: absolute;
+            top: 50%;
+            button {
+                background-color: transparent;
+                border-radius: 1.3rem;
+                padding: .7rem;
                 color: white;
-                border: 1px solid white;
-                position: absolute;
-                top: 50%;
-                left: 50%;
-                transform: translate(-50%, -50%);
-                border-radius: 50%;
-                width: 50px;
-                height: 50px;
-                box-sizing: border-box;
-           }
+                border: 2px solid white;
+                margin-top: 1rem;
+                font-weight: 600;
+            }
         }
     }
-}
+    
+    
+}    
 </style>
-
